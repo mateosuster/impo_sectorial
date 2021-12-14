b9157b555492a3e28bf97e84b08e7209e4f53774
mateosuster <56704115+mateosuster@users.noreply.github.com>
2021-12-14 17:56:12 -0300

    inicio emprolijamiento script clasificador sisd

--- scripts/nivel_ncm_12d_6act/3_clasificador_nivel_ncm_12d_6act.py
+++ scripts/nivel_ncm_12d_6act/3_clasificador_nivel_ncm_12d_6act.py
@@ -9,12 +9,8 @@ Created on Tue Jun 29 11:04:54 2021
 # Directorio de trabajo y librerias
 # =============================================================================
 import os 
-#Mateo
 #os.chdir("C:/Archivos/repos/impo_sectorial/scripts/nivel_ncm_12d_6act")
-#igal
 # os.chdir("C:/Users/igalk/OneDrive/Documentos/laburo/CEP/procesamiento impo/nuevo1/impo_sectorial/scripts/nivel_ncm_12d_6act")
-
-#VM
 os.chdir("D:/impo_sectorial/impo_sectorial/scripts/nivel_ncm_12d_6act")
 
 # os.getcwd()
@@ -34,11 +30,9 @@ from pre_visualizacion_nivel_ncm_12d_6act import *
 
 
 
-
 #############################################
 # Cargar bases con las que vamos a trabajar #
 #############################################
-#datasetes utilizados en ultimas corridas 
 #impo_d12 = pd.read_csv("../data/impo_2017_diaria.csv") #falta
 clae = pd.read_csv( "../data/clae_nombre.csv")
 comercio = pd.read_csv("../data/comercio_clae.csv", encoding="latin1")
@@ -49,28 +43,12 @@ ncm12_desc = pd.read_csv("../data/d12_2012-2017.csv", sep=";")
 dic_stp = pd.read_excel("../data/bsk-prod-clasificacion.xlsx")
 data_predichos = pd.read_csv("../data/resultados/datos_clasificados_modelo_all_data.csv", sep = ";")
 datos_clasificados = pd.read_csv("../data/resultados/data_modelo_diaria.csv") #falta
+
 # CIIU
 dic_ciiu = pd.read_excel("../data/Diccionario CIIU3.xlsx")
 clae_to_ciiu = pd.read_excel("../data/Pasar de CLAE6 a CIIU3.xlsx")
 hs_to_isic = pd.read_csv("../data/JobID-64_Concordance_HS_to_I3.csv", encoding = "latin" )
 
-
-#impo 12 d
-# impo_d12 = pd.read_csv("../data/IMPO_17_feature.csv")
-# impo_d12 = pd.read_csv("../data/IMPO_2017_12d.csv")
-#impo_17 = pd.read_csv(  "../data/IMPO_2017.csv", sep=";")
-
-#cuit_clae = pd.read_csv( "../data/cuit 2017 impo_con_actividad.csv")
-# bec = pd.read_csv( "../data/HS2012-17-BEC5 -- 08 Nov 2018.csv")
-# bec_to_clae = pd.read_csv("../data/bec_to_clae.csv")
-#diccionario ncm12d
-# ncm12_desc = pd.read_csv("../data/NCM 12d.csv", sep=";")
-# ncm12_desc_split = pd.concat([ncm12_desc.iloc[:,0], pd.DataFrame(ncm12_desc['Descripción Completa'].str.split('//', expand=True))], axis=1)
-
-# parts_acces  =pd.read_excel("C:/Archivos/Investigación y docencia/Ministerio de Desarrollo Productivo/balanza comercial sectorial/tablas de correspondencias/nomenclador_28052021.xlsx", names=None  , header=None )
-# transporte_reclasif  = pd.read_excel("C:/Archivos/Investigación y docencia/Ministerio de Desarrollo Productivo/balanza comercial sectorial/tablas de correspondencias/resultados/bec_transporte (reclasificado).xlsx")
-
-# bce_cambiario = pd.read_csv("../data/balance_cambiario.csv", skiprows = 3, error_bad_lines=False, sep= ";", na_values =['-'])
 
 #############################################
 #           preparación bases               #
