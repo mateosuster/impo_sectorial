df87162d7dbd3724a630a447f11b2b7658077e1d
mateosuster <mateosuster@gmail.com>
2022-01-28 19:34:04 +0000

    que traeen las entidades bancarias

--- scripts/nivel_ncm_12d_6act/3_clasificador_nivel_ncm_12d_6act.py
+++ scripts/nivel_ncm_12d_6act/3_clasificador_nivel_ncm_12d_6act.py
@@ -1,3 +1,4 @@
+
 # =============================================================================
 # Directorio de trabajo y librerias
 # =============================================================================
@@ -48,6 +49,9 @@ datos = preprocesamiento_datos(datos, dic_propio)
 datos_bk , datos_bk_sin_picks, bk_picks = asignacion_stp_BK(datos, dic_stp)
 datos_ci = filtro_ci(datos)
 
+#que trae intermediación financiera ?
+datos_bk = datos_bk.merge(ncm12_desc, how = "left", left_on = "HS6_d12", right_on="HS_12d")
+data_j = datos_bk[datos_bk["letra1"]=="J"].sort_values("valor", ascending=False)
 #############################################
 #         BK                                #
 #############################################
